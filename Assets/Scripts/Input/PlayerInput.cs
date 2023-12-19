d284c440c781ee3c6404fa775135db27f3c3a517
최유섭 <80661998+9503chl@users.noreply.github.com>
2023-12-19 16:31:08 +0900

    23/12/19
    2D , 3D 캔버스 분리.

--- Assets/Scripts/Input/PlayerInput.cs
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -14,10 +14,10 @@ public class PlayerInput : BaseInput
         #region 캐릭터 상호작용
         if (Player != null)//아직 화면 벗어나기 설정안함
         {
-            if (Input.GetKey(KeyCode.W)) Player.transform.Translate(0, PlayerComponent.Speed * Time.deltaTime, 0);
-            if (Input.GetKey(KeyCode.S)) Player.transform.Translate(0, -PlayerComponent.Speed * Time.deltaTime, 0);
-            if (Input.GetKey(KeyCode.A)) Player.transform.Translate(-PlayerComponent.Speed * Time.deltaTime, 0, 0);
-            if (Input.GetKey(KeyCode.D)) Player.transform.Translate(PlayerComponent.Speed * Time.deltaTime, 0, 0);
+            if (Input.GetKey(KeyCode.W)) Player.transform.Translate(0, PlayerComponent.Speed * Time.deltaTime * 10, 0);
+            if (Input.GetKey(KeyCode.S)) Player.transform.Translate(0, -PlayerComponent.Speed * Time.deltaTime * 10, 0);
+            if (Input.GetKey(KeyCode.A)) Player.transform.Translate(-PlayerComponent.Speed * Time.deltaTime * 10, 0, 0);
+            if (Input.GetKey(KeyCode.D)) Player.transform.Translate(PlayerComponent.Speed * Time.deltaTime * 10, 0, 0);
         }
         #endregion
 
